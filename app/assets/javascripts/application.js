d01defb16998ac171cce8dd99843c260c41c5aa7
kisemy <kisemyj@gmail.com>
2023-05-13 11:57:51 +0300

    My first File

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require flowdash
+//= require flowdash
+
+//= require jquery-ui
 
 //import Rails from "@rails/ujs"
 //import Turbolinks from "turbolinks"
@@ -14,4 +16,4 @@
 //Turbolinks.start()
 //ActiveStorage.start()
 
-//= require flowdash
+// require flowdash
